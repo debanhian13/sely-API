ee2976a2634b46121b834315094afae65c79eeb9
debanhian13 <debanhian@hotmail.com>
2017-12-06 13:19:35 -0600

    2Url

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,6 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 mongoose.connect('mongodb://debanhi:123@ds119406.mlab.com:19406/sely', { useMongoClient: true });
 
-// My Libraries 
-// ...
 
 // Constants
 var router = express.Router();
@@ -23,19 +21,15 @@ var ArtistasSchema = mongoose.Schema({
 
 var ArtistasUrl = mongoose.model('artistas', ArtistasSchema, 'artistas');
 
-/*router.route('/ArtistasUrl').get(function(req,res)
-	{
-		ArtistasUrl.find({},function(err, ArtistasUrl){
-			if (err) return console.log(err);
-			res.json(ArtistasUrl);
-			console.log(ArtistasUrl);
-		})
+var AlbumSchema = mongoose.Schema({
+	name: String,
+	url: String
+});
 
-	})*/
+var AlbumUrl = mongoose.model('albums',AlbumSchema, 'albums');
 
 
 // Routes
-//module.exports = router;
 module.exports = function(app){
 
   app.get('/', function(req, res, next){return res.status(200).json({name: "hello"}).end();})
@@ -47,6 +41,15 @@ module.exports = function(app){
 			res.json(ArtistasUrl);
 			console.log(ArtistasUrl);
 		})
+	})
 
+
+   app.get('/AlbumUrl', function(req,res)
+	{
+		AlbumUrl.find({},function(err, AlbumUrl){
+			if (err) return console.log(err);
+			res.json(AlbumUrl);
+			console.log(AlbumUrl);
+		})
 	})
 };
